96df1b8f984f604349ffac8b3e80e452bba59c01
Denis Listiadi <denislistiadi24@gmail.com>
2021-03-08 00:37:12 +0700

    update

--- src/server.js
+++ src/server.js
@@ -19,5 +19,7 @@ app.use("/api/v1/users", user);
 
 app.use(errorHandler);
 
+app.get('/', (req, res) => res.status(200).send("Hello"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started at http://localhost:${PORT}`));
